19e661beba5d4b3251b653aca0a73eb974f7030a
Johan Rydell <johannilsrydell89@gmail.com>
2024-11-27 10:07:25 +0100

    chapter two

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function aboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>YO!</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -16,11 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
     color: "#fff",
   },
 });
