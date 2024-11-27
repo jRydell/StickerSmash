ae24c760e8b9d37710e174f9315b4b640fa929ee
Johan Rydell <johannilsrydell89@gmail.com>
2024-11-27 14:06:57 +0100

    chapter 3 done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
 
+import ImageViewer from "@/components/ImageViever";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("../../assets/images/background-image.png");
+
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>YO!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Choose a photo"} theme={"primary"} />
+        <Button label={"Use this photo"} theme={"primary"} />
+      </View>
     </View>
   );
 }
@@ -13,14 +24,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
